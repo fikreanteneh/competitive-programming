194ad97d0dec7dc48ca6acc41809118f404ec1ec
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2022-11-03 23:37:13 +0300

    https://leetcode.com/problems/next-greater-node-in-linked-list/

---
+++ next-greater-node-in-linked-list.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def nextLargerNodes(self, head: Optional[ListNode]) -> List[int]:
+        stack = []
+        ans = []
+        i = 0
+        while head:
+            ans.append(0)
+            while stack and stack[-1][0] < head.val:
+                ans[stack[-1][1]] = head.val
+                stack.pop()
+            stack.append((head.val, i))
+            head = head.next
+            i += 1
+        return ans
