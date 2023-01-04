41344c13da0c6ce2fd431fa80cc6187a752777b3
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2023-01-04 17:57:21 +0300

    Time: 74 ms (56.68%), Space: 13.9 MB (50.69%) - LeetHub

--- 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.py
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.py
@@ -3,22 +3,20 @@ class Solution:
         queens = set(tuple(i) for i in queens)
         x, y = king[0], king[1]
         answer = []
-        def is_bound(row, col):
-            return 0 <= row < 8 and 0 <= col < 8
-        def recursive(row, col):
-            nonlocal x
-            nonlocal y
-            new_x  = x
-            new_y = y
-            while is_bound(new_x, new_y):
-                new_x += row
-                new_y += col
-                if (new_x, new_y) in queens:
-                    answer.append([new_x, new_y])
-                    break
         DIR = [(1, 1), (1, 0), (0, 1), (-1, -1), (-1, 0), (0, -1), (-1, 1), (1, -1)]
         for row, col in DIR:
-            recursive(row, col)
+            self.recursive(row, col, answer, x, y, queens)
         return answer
+    
+    def is_bound(self, row, col):
+        return 0 <= row < 8 and 0 <= col < 8
+        
+    def recursive(self, row, col, answer, new_x, new_y, queens):
+        while self.is_bound(new_x, new_y):
+            new_x += row
+            new_y += col
+            if (new_x, new_y) in queens:
+                answer.append([new_x, new_y])
+                break
     
         
