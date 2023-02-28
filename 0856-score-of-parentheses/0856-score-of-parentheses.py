3242b65ea1d9ea4c690ff2702f1b2c50830904f6
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2023-02-28 14:59:31 +0300

    Time: 35 ms (46.00%), Space: 13.8 MB (49.79%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.py
+++ 0856-score-of-parentheses/0856-score-of-parentheses.py
@@ -4,24 +4,22 @@ class Solution:
         for i, par in enumerate(s):
             if par == "(":
                 stack.append(False)
+                continue
+            appended = [0,"add"]
+            while stack[-1]: 
+                appended[0] += stack[-1][0]
+                stack.pop()
+            stack.pop()
+            if not appended[0]: 
+                appended[0] += 1
             else:
-                if not stack[-1]:
-                    stack.pop()
-                    stack.append([1,"add"])
-                else:
-                    sums = 0
-                    while stack[-1] and stack[-2]: 
-                        stack[-2][0] += stack[-1][0]
-                        stack.pop()
-                    y = stack.pop()
-                    stack.pop()
-                    stack.append([y[0] * 2, "add"])
-            # print(stack)  
+                appended[0] *= 2
+            stack.append(appended)
         ans = 0
         for i in stack:
             ans += i[0]
         return ans
-   
+   # (((())))
 # 0 1 2 
 # 1
 # 1
