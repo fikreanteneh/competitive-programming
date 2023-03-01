24ac568cf974ea207f4358e53ae62aba62153c93
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2023-03-01 10:29:51 +0300

    Time: 46 ms (25.55%), Space: 13.8 MB (98.63%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -2,34 +2,28 @@ class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         head = ListNode(None)
         new = head
-        while list1 and list2:
-            if list1.val <= list2.val:
-                new.next = list1
-                list1 = list1.next
-            else:
-                new.next = list2
-                list2 = list2.next
-            new = new.next
-        
-        temp = list1 if list1 else list2
-        while temp:
-            new.next = temp
-            temp = temp.next
-            new = new.next
+        self.solution(new, list1, list2)
         return head.next
-#             head = ListNode()
-#             tail = head
-#             while list1 and list2:
-#                 if list1.val <= list2.val:
-#                     new = ListNode(list1.val)
-#                     tail.next = new
-#                     list1 = list1.next
-#                 else:
-#                     new = ListNode(list2.val)
-#                     tail.next = new
-#                     list2 = list2.next
-#                 tail = tail.next
-                
-#             if list1: tail.next = list1
-#             if list2: tail.next = list2
-#             return head.next
+    
+    
+    def solution(self, head, list1, list2):
+        if not list1 or not list2:
+            temp = list1 if not list2 else list2
+            
+            while temp:
+                head.next = temp
+                temp = temp.next
+                head = head.next 
+            return
+        
+        if list1.val <= list2.val:
+            head.next = list1
+            list1 = list1.next
+            
+        else:
+            head.next = list2
+            list2 = list2.next
+            
+        head = head.next
+        self.solution(head, list1, list2)
+            
