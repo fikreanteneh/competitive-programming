534d5a8fd70e083a941c21adf050c499870b9897
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2023-03-01 10:19:08 +0300

    Time: 208 ms (48.33%), Space: 43.3 MB (5.48%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,8 +3,10 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        n = len(s) - 1
-        for i in range((n+1)//2):
-            s[i], s[n - i] = s[n-i], s[i]
-        return s
-            
+        self.solution(s, 0, len(s)-1)
+    
+    def solution(self, s, l, r):
+        if l >= r:
+            return
+        s[l], s[r] = s[r], s[l]
+        self.solution(s, l + 1, r - 1)
