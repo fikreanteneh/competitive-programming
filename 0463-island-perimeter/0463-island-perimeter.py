e0afe452cbf049bcf99fbc2825847f73077527d3
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2023-03-05 12:26:21 +0300

    Time: 474 ms (85.44%), Space: 14.2 MB (88.19%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -5,24 +5,16 @@ class Solution:
             i.append(0)
         grid.insert(0, [0] * (len(grid[0]) ))
         grid.append([0] * (len(grid[0]) ))
-        bound =[0] #{1:0, 2:0, 3:0, 4:0}
+        
+        perimeter = 0
         
         for i in range(1,len(grid)-1):
             for j in range(1, len(grid[0])-1):
                 if grid[i][j]:
-                    self.calculator(grid, i, j, bound)
-        
-        return bound[0]
-        
-        
-    def calculator(self, grid, i, j, bound):
-        if not grid[i-1][j]:
-            bound[0] += 1
-        if not grid[i+1][j]:
-            bound[0] += 1
-        if not grid[i][j-1]:
-            bound[0] += 1
-        if not grid[i][j + 1]:
-            bound[0] += 1
-            
+                    perimeter += (1 - grid[i-1][j])
+                    perimeter += (1- grid[i+1][j])
+                    perimeter += (1 - grid[i][j-1])
+                    perimeter += ( 1- grid[i][j + 1])
+        return perimeter
+    
             
