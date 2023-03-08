77e0ee5250fee467c3b335b622b2c81f2f637961
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2023-03-08 15:27:43 +0300

    Time: 42 ms (16.95%), Space: 13.9 MB (15.25%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -8,23 +8,37 @@ class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         if not root:
             return []
-        def solution(roots, answer):
-            temp = []
-            newRoots = []
-            for root in roots:
-                temp.append(root.val)
-                if root.left:
-                    newRoots.append(root.left)
-                if root.right:
-                    newRoots.append(root.right)
-            answer.append(temp)
-            if newRoots:
-                solution(newRoots, answer)
-            
+        
+        def solution(root, answer, curr, maxi):
+            if not root:
+                return
+            if curr > maxi[0]:
+                answer.append(root.val)
+                maxi[0] = curr
+            solution(root.right, answer, curr + 1, maxi)
+            solution(root.left, answer, curr + 1, maxi)
+        
         answer = []
-        solution([root], answer)
-        new = []
-        for i in answer:
-            new.append(i[-1])
-        return new
+        maxi = [0]
+        solution(root, answer, 1,  maxi)
+        return answer
+#         def solution(roots, answer):
+#             temp = []
+#             newRoots = []
+#             for root in roots:
+#                 temp.append(root.val)
+#                 if root.left:
+#                     newRoots.append(root.left)
+#                 if root.right:
+#                     newRoots.append(root.right)
+#             answer.append(temp)
+#             if newRoots:
+#                 solution(newRoots, answer)
+            
+#         answer = []
+#         solution([root], answer)
+#         new = []
+#         for i in answer:
+#             new.append(i[-1])
+#         return new
         
