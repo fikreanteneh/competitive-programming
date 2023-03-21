106dd63672ff6ddfa86dc642720c74a7a416e0fc
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2023-03-21 18:20:06 +0300

    Time: 58 ms (63.86%), Space: 15.4 MB (31.66%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -6,19 +6,17 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        stack = []
         store = defaultdict(int)
         self.answer= 0
         def solution(root, sums):
             if not root:
                 return 0
-            stack.append(sums)
             store[sums] += 1
             sums += root.val
             check = sums - targetSum
             self.answer += store[check]
             solution(root.left, sums)
             solution(root.right, sums)
-            store[stack.pop()] -= 1
+            store[sums - root.val] -= 1
         solution(root, 0)
         return self.answer
