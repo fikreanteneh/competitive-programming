11fbb33bdc32b5345de49875bfdac756c7528689
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2023-04-04 18:01:25 +0300

    Time: 35 ms (73.87%), Space: 13.8 MB (95.68%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -2,10 +2,10 @@ class Solution:
     def minSteps(self, n: int) -> int:
         operation = 0
         while n > 1:
-            i = 2
+            i = n// 2
             while n % i != 0:
-                i += 1
-            operation += i
-            n //= i
+                i -= 1
+            operation += (n//i)
+            n = i
         return operation
         
