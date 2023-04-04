06d17d10c9b877a2c8473f6fe3cf898c4f6002d0
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2023-04-04 07:44:43 +0300

    Time: 5873 ms (21.62%), Space: 52.8 MB (74.28%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -2,16 +2,14 @@ class Solution:
     def countPrimes(self, n: int) -> int:
         if n < 3:
             return 0
-        num = [i for i in range(n)]
+        num = [True] * n
+        num[0], num[1] = False, False
         r = int(sqrt(n)) + 1
         for i in range(2, r):
             if num[i]:
                 for j in range(i, n//i + 1):
                     x = i * j
                     if x  < n:
-                        num[x] = 0
-        num = set(num)
-        num.discard(0)
-        num.discard(1)
-        return len(num)
+                        num[x] = False
+        return Counter(num)[True]
             
