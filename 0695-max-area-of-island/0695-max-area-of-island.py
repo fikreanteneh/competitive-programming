3a843eeaa0402b76596e04655ed0fd6a0c9b3bec
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2023-04-10 18:23:05 +0300

    Time: 128 ms (85.85%), Space: 17.5 MB (40.80%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -8,21 +8,20 @@ class Solution:
         grid.append([0] * m)
         
         self.answer = 0
-        visited = set()
         
         def solution(row, col):
             curr = 1
             dix = [(row + 1, col), (row -1, col), (row, col + 1), (row, col - 1)]
-            visited.add((row, col))
+            grid[row][col] = 0
             for x, y in dix:
-                if grid[x][y] and (x, y) not in visited:
+                if grid[x][y]:
                     curr += solution(x, y)
             
             return curr
         
         for i in range(1, n-1):
             for j in range(1, m-1):
-                if grid[i][j] and (i,j) not in visited:
+                if grid[i][j]:
                     area = solution(i,j)
                     self.answer = max(self.answer, area)
         
