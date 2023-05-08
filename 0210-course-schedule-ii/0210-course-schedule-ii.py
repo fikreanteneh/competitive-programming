cd5619c2bd94c4aa3751feeb4936b675afac28d1
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2023-05-08 19:09:13 +0300

    Time: 129 ms (12.32%), Space: 19.9 MB (6.80%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -6,18 +6,42 @@ class Solution:
             graph[course][0] += 1
             graph[pre][1].append(course)
         answer = []
-        que = deque([])
-        for node, val in graph.items():
-            if not val[0]:
-                que.append(node)
-        while que:
-            node = que.pop()
-            answer.append(node)
+        visited = set()
+        def solution(node, visited):
+            if node in visited:
+                return True
+            visited.add(node)
             for child in graph[node][1]:
-                graph[child][0] -= 1
-                if graph[child][0] == 0:
-                    que.appendleft(child)    
-        if len(answer) != numCourses:
-            return []
+                if graph[child] and solution(child, visited):
+                    return True
+            
+            answer.append(node)
+            graph[node] = None
+        
+        
+        for node in range(numCourses):
+            if graph[node] and solution(node, set()):
+                return []
+        answer.reverse()
         return answer
+        
+        # graph = {i: [0, []] for i in range(numCourses)}
+        # for course, pre in prerequisites:
+        #     graph[course][0] += 1
+        #     graph[pre][1].append(course)
+        # answer = []
+        # que = deque([])
+        # for node, val in graph.items():
+        #     if not val[0]:
+        #         que.append(node)
+        # while que:
+        #     node = que.pop()
+        #     answer.append(node)
+        #     for child in graph[node][1]:
+        #         graph[child][0] -= 1
+        #         if graph[child][0] == 0:
+        #             que.appendleft(child)    
+        # if len(answer) != numCourses:
+        #     return []
+        # return answer
             
