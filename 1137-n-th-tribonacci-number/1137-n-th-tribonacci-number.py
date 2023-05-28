916fe5ce8ed36180764c29470cf76f1421b27e4d
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2023-05-28 19:02:44 +0300

    Time: 40 ms (46.29%), Space: 16.3 MB (34.38%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -3,11 +3,8 @@ class Solution:
         
         mapp = [0,1,1]
         memo = {}
-        def calc(n):
-            if n <= 2:
-                return mapp[n]
-            if n not in memo:
-                memo[n] = calc(n-1) + calc(n-2) + calc(n-3)
-            return memo[n]
-        
-        return calc(n)
+        if n <= 2:
+            return mapp[n]
+        for i in range(3, n + 1):
+            mapp.append(mapp[i - 1] + mapp[i - 2] + mapp[i - 3])
+        return mapp[n]
