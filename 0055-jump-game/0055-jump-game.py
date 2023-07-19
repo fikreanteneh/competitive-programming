f3b963e66d8c1dd277599e42d375911fb69eea4f
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2023-07-19 23:25:23 +0300

    Time: 9438 ms (5.00%), Space: 57.6 MB (5.01%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -2,10 +2,13 @@ class Solution:
     def canJump(self, nums: List[int]) -> bool:
         # store = set()
         n = len(nums) - 1
+        
         @cache
         def sol(index):
             if index == n:
                 return True
+            elif index > n:
+                return False
             for i in range(1,nums[index] + 1):
                 if sol(index + i):
                     return True
