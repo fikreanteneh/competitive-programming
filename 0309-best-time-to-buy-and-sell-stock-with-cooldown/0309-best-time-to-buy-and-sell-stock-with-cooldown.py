db749e68be71b4664985515525375b0b2fdb4f45
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2023-07-22 19:53:34 +0300

    Time: 51 ms (91.69%), Space: 16.6 MB (91.71%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,19 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        last = {"cool": 0, "buy": -prices[0], "sell": 0}
+        last = [0, -prices[0], 0]
         for i in range(1, len(prices)):
             price = prices[i]
-            new = {}
-            for prev, state in last.items():
-                if prev == "buy":
-                    new["sell"] = state + price
-                elif prev == "sell" :
-                    new["cool"] = max(state, last["cool"])
-                elif prev == "cool":
-                    new["buy"] = max(last["buy"], state - price)
+            new = []
+            new.append(max(last[2], last[0]))
+            new.append(max(last[1], last[0] - price))
+            new.append(last[1] + price)
             last = new
         
-        return max(last.values())
+        return max(last)
             
         
 #    -1 buy   1 sell  1 cool   1 buy   2 cell
