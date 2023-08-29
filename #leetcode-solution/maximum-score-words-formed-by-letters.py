6b2377c253bf710f3d6071b8c57e8cd3b41f4ca7
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2023-08-29 16:32:45 +0300

    Added solution for Maximum Score Words Formed by Letters

---
+++ #leetcode-solution/maximum-score-words-formed-by-letters.py
@@ -0,0 +1,36 @@
+class Solution:
+    def maxScoreWords(self, words: List[str], letters: List[str], score: List[int]) -> int:
+        n = len(words)
+        letters = Counter(letters)
+        visited = set()
+        
+        def contain(word):
+            point = 0
+            flag = False
+            for letter in word:
+                if letters[letter] == 0:
+                    flag = True  
+                point += score[ord(letter) - ord("a")]
+                letters[letter] -= 1
+            if flag:
+                return (False, 0)
+            return (True, point)
+            
+            
+        def uncontain(word):
+            for letter in word:
+                letters[letter] += 1
+        
+        def dfs(prev, index):
+            ans = prev
+            for i in range(index,n):
+                if i not in visited:
+                    visited.add(i)
+                    possible, point = contain(words[i])
+                    if possible :   
+                        ans = max(ans, dfs(point + prev, i + 1), point + prev)
+                    uncontain(words[i])
+                    visited.discard(i)
+            return ans
+        
+        return dfs(0, 0)
