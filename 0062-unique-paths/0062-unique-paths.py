9eadd88a2217f7677d0cf181469d01169d45cecb
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2023-09-03 18:25:13 +0300

    Time: 42 ms (52.62%), Space: 16.2 MB (95.35%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,27 +1,26 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        memo = defaultdict(int)
-        memo[(m-1, n-1)] = 1
-        def calc(node):
-                if node in memo:
-                    return memo[node]
-                x, y = node
-                if x < m:
-                    memo[node] += calc((x + 1, y))
-                if y < n:
-                    memo[node] += calc((x, y + 1))
-                return memo[node]
+#         memo = defaultdict(int)
+#         memo[(m-1, n-1)] = 1
+#         def calc(node):
+#                 if node in memo:
+#                     return memo[node]
+#                 x, y = node
+#                 if x < m:
+#                     memo[node] += calc((x + 1, y))
+#                 if y < n:
+#                     memo[node] += calc((x, y + 1))
+#                 return memo[node]
         
-        return calc((0,0))
+#         return calc((0,0))
     
     
-        # path = [[0 for i in range(n)] for j in range(m)]
-        # path[0][0] = 1
-        # for i in range(m):
-        #     for j in range(n):
-        #         if j > 0:
-        #             path[i][j] += path[i][j-1]
-        #         if i > 0:
-        #             path[i][j] += path[i-1][j]
-        #         print(path)
-        # return path[m-1][n-1]
+        path = [[0 for i in range(n)] for j in range(m)]
+        path[0][0] = 1
+        for i in range(m):
+            for j in range(n):
+                if j > 0:
+                    path[i][j] += path[i][j-1]
+                if i > 0:
+                    path[i][j] += path[i-1][j]
+        return path[m-1][n-1]
