5874e9d7afdaf6acde27d4752f99815b7f5f51f6
Fikremariam Anteneh Asegie <105011492+fikreanteneh@users.noreply.github.com>
2023-09-13 09:51:37 +0300

    Time: 156 ms (35.61%), Space: 19.2 MB (91.55%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,14 +1,22 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
+        
         n = len(ratings)
-        arr = [1] * n
+        answer = [1] * n
+        
         for i in range(1, n):
-            if arr[i] <= arr[i - 1] and ratings[i] > ratings[i - 1]:
-                arr[i] += (arr[i - 1] - arr[i] + 1)
-                
+            if ratings[i] > ratings[i - 1]:
+                answer[i] += answer[i - 1]
+        
         for i in range(n - 2, -1, -1):
-            if arr[i] <= arr[i + 1] and ratings[i] > ratings[i + 1]:
-                arr[i] += (arr[i + 1] - arr[i] + 1)
-        return sum(arr)
+            if ratings[i] > ratings[i + 1]:
+                answer[i] = max(answer[i], answer[i + 1] + 1 )
+        
+        
+        return sum(answer)
+        
+        
             
-                
+            
+        
+        
