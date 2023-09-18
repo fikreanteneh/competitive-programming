f01e52b7249e2f91f118b98b08bd1f3831200516
Fikremariam Anteneh Asegie <105011492+fikreanteneh@users.noreply.github.com>
2023-09-18 16:12:07 +0300

    Added solution for 2 Keys Keyboard

--- #leetcode-solution/2-keys-keyboard.py
+++ #leetcode-solution/2-keys-keyboard.py
@@ -1,10 +1,14 @@
 class Solution:
     def minSteps(self, n: int) -> int:
+        
+        
+        # Math Solution
         operation = 0
         while n > 1:
             copiedFrom = n // 2
             while n % copiedFrom:
                 copiedFrom -= 1
-            operation += (n // copiedFrom)
-            n = copiedFrom
+            operation += 1 # for copying
+            operation +=  (n//copiedFrom - 1) # for pasting
+            n = copiedFrom # next goal is reching this value
         return operation
