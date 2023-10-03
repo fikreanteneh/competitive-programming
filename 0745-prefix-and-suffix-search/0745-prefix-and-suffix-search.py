3458756b5694041566b7ac46b1e55b14007a1bca
Fikremariam Anteneh Asegie <105011492+fikreanteneh@users.noreply.github.com>
2023-10-03 15:49:20 +0300

    Time: 2072 ms (49.81%), Space: 69.8 MB (60.31%) - LeetHub

--- 0745-prefix-and-suffix-search/0745-prefix-and-suffix-search.py
+++ 0745-prefix-and-suffix-search/0745-prefix-and-suffix-search.py
@@ -1,9 +1,65 @@
+# class Node:
+#     def __init__(self):
+#         self.child = [None]*27
+#         self.end = False
+#         self.lastNode = None
+#         self.lastIndex = -1
+
+# class Trie:
+
+#     def __init__(self):
+#         self.root = Node()
+    
+#     def charToIndex(self, char):
+#         return ord(str(char)) - 97
+    
+#     def indexToChar(self, index):
+#         return chr(index + 97)
+
+#     def reachNode(self, value, root = None):
+#         curr = root if root else self.root
+#         parent, index = curr, -1
+#         for i, val in enumerate(value):
+#             index = self.charToIndex(val)
+#             if curr.child[index] is None:
+#                 return (None, None, -1)
+#             parent = curr
+#             curr = curr.child[index]
+#         return (curr, parent, index)
+
+#     def insert(self, value, wordIndex) -> None:
+#         curr = self.root
+#         for i, val in enumerate(value):
+#             index = self.charToIndex(val)
+#             if curr.child[index] is None:
+#                 curr.child[index] = Node()
+#             curr.lastIndex = wordIndex
+#             curr.lastNode = curr.child[index]
+#             curr = curr.child[index]
+#         curr.end = True
+#         curr.lastIndex = wordIndex
+
+# class WordFilter:
+
+#     def __init__(self, words: List[str]):
+#         self.tree = Trie()
+#         for i, word in enumerate(words):
+#             for j in range(len(word) - 1, -1, -1):
+#                 self.tree.insert(word[j:] + "{" + word, i)
+
+#     def f(self, pref: str, suff: str) -> int:
+#         word = suff  + "{" + pref
+#         curr, parent, index = self.tree.reachNode(word)
+#         if not curr:
+#             return -1
+#         return curr.lastIndex
+
+        
 class Node:
     def __init__(self):
-        self.child = [None]*27
+        self.child = [None]*26
         self.end = False
-        self.lastNode = None
-        self.lastIndex = -1
+        self.indexes = []
 
 class Trie:
 
@@ -26,33 +82,45 @@ class Trie:
             parent = curr
             curr = curr.child[index]
         return (curr, parent, index)
-
-    def insert(self, value, wordIndex) -> None:
+ 
+    def insert(self, value, weight) -> None:
         curr = self.root
         for i, val in enumerate(value):
             index = self.charToIndex(val)
             if curr.child[index] is None:
                 curr.child[index] = Node()
-            curr.lastIndex = wordIndex
-            curr.lastNode = curr.child[index]
+            curr.indexes.append(weight)
             curr = curr.child[index]
         curr.end = True
-        curr.lastIndex = wordIndex
-
+        curr.indexes.append(weight)
+        
 class WordFilter:
 
     def __init__(self, words: List[str]):
-        self.tree = Trie()
+        self.prefix = Trie()
+        self.suffix = Trie()
         for i, word in enumerate(words):
-            for j in range(len(word) - 1, -1, -1):
-                self.tree.insert(word[j:] + "{" + word, i)
+            self.prefix.insert(word, i)
+            self.suffix.insert(word[::-1], i)
 
     def f(self, pref: str, suff: str) -> int:
-        word = suff  + "{" + pref
-        curr, parent, index = self.tree.reachNode(word)
-        if not curr:
-            return -1
-        return curr.lastIndex
+        prefix = self.prefix.reachNode(pref)[0]
+        suffix = self.suffix.reachNode(suff[::-1])[0]
+        prefix = [] if not prefix else prefix.indexes
+        suffix = [] if not suffix else suffix.indexes
+        n, m = len(prefix) - 1, len(suffix) - 1
+        while n >= 0 and m >= 0:
+            if prefix[n] == suffix[m]:
+                return prefix[n]
+            elif prefix[n] > suffix[m]:
+                n -= 1
+            else:
+                m -= 1
+        return -1
+        
+
+
+
 # Your WordFilter object will be instantiated and called as such:
 # obj = WordFilter(words)
 # param_1 = obj.f(pref,suff)
