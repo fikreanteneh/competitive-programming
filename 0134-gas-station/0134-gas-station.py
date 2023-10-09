424c51606f3e47634f1c54a2e6f5ec867f84b1ed
Fikremariam Anteneh Asegie <105011492+fikreanteneh@users.noreply.github.com>
2023-10-09 18:13:30 +0300

    Time: 1073 ms (50.97%), Space: 22.2 MB (39.94%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,19 +1,14 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        if gas == [6,1,4,3,5] and cost == [3,8,2,4,2]:
-            return 2
         n = len(gas)
         prefix = 0
         left = 0
         for right in range(n):
-            new = (gas[right] - cost[right]) + prefix
-            # print(prefix, new)
-            if (new < prefix and new < 0):
-                # print("---")
+            prefix += (gas[right] - cost[right])
+            if (prefix < 0):
                 left = right + 1
-            
-            prefix = new
-        return left if prefix >= 0 else -1
+                prefix = 0
+        return left if sum(gas)>= sum(cost) else -1
             
             
             
