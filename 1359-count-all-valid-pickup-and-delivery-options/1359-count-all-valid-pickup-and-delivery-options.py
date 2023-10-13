5703cf8990c6c3ce2c97c779c8769cb6a61720df
Fikremariam Anteneh Asegie <105011492+fikreanteneh@users.noreply.github.com>
2023-10-13 09:11:58 +0300

    Time: 43 ms (31.30%), Space: 16.3 MB (67.43%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -2,13 +2,14 @@ class Solution:
     def countOrders(self, n: int) -> int:
         
         answer = 1
-        
+        MOD = (10**9 + 7 )
         for i in range(1, n + 1):
             prevPickDeliver = ((i - 1) << 1) + 1
             order = (prevPickDeliver *(1 + prevPickDeliver)) >> 1 
             answer *= order
+            answer %= MOD
         
-        return answer % (10**9 + 7 )
+        return answer
         
         
                      
