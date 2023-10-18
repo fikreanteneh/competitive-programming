5974f9af27a37433be1b6fe30697abcaf1e701d4
Fikremariam Anteneh Asegie <105011492+fikreanteneh@users.noreply.github.com>
2023-10-18 15:52:28 +0300

    Added solution for Unique Paths

---
+++ #leetcode-solution/unique-paths.py
@@ -0,0 +1,26 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        return comb( (m + n - 2), (min(m, n) - 1) ) 
+#         memo = defaultdict(int)
+#         memo[(m-1, n-1)] = 1
+#         def calc(node):
+#                 if node in memo:
+#                     return memo[node]
+#                 x, y = node
+#                 if x < m:
+#                     memo[node] += calc((x + 1, y))
+#                 if y < n:
+#                     memo[node] += calc((x, y + 1))
+#                 return memo[node]
+        
+#         return calc((0,0))
+    
+        # path = [[0 for i in range(n)] for j in range(m)]
+        # path[0][0] = 1
+        # for i in range(m):
+        #     for j in range(n):
+        #         if j > 0:
+        #             path[i][j] += path[i][j-1]
+        #         if i > 0:
+        #             path[i][j] += path[i-1][j]
+        # return path[m-1][n-1]
