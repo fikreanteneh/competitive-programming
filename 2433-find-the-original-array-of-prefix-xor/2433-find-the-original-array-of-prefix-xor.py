d39cef6bfecbcab13c01d5806005bd6d84bf013c
Fikremariam Anteneh Asegie <105011492+fikreanteneh@users.noreply.github.com>
2023-10-31 22:10:27 +0300

    Time: 661 ms (71.45%), Space: 31.2 MB (96.56%) - LeetHub

--- 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.py
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.py
@@ -1,15 +1,7 @@
 class Solution:
     def findArray(self, pref: List[int]) -> List[int]:
-        
-        def calc(pref1, pref2):
-            num = ""
-            for i in range(21, -1, -1):
-                p1 = (pref1 >> i) & 1
-                p2 = (pref2 >> i) & 1
-                num += str( p1^p2  )
-            return int(num, 2)
         curr = 0
         for i, val in enumerate(pref):
-            pref[i] = calc(curr, val)
+            pref[i] = curr^val
             curr = val
         return pref
