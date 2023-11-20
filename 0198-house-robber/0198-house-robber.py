c45dbc3415f05d27ebceff6842bd3c8c1f171fe7
Fikremariam Anteneh Asegie <105011492+fikreanteneh@users.noreply.github.com>
2023-11-20 14:35:22 +0300

    Time: 44 ms (21.44%), Space: 16.5 MB (13.94%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,18 +1,26 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        memo = {}
-        
-        
-        def calc(house):
-            if house >= n:
-                return 0
+        if n <= 2:
+            return max(nums)
+        store = [None]*n
+        sums = nums.copy()
+        for i in range(n):
+            curr = (0, None)
+            val = nums[i]
+            for j in range(i - 2, -1, -1):
+                if sums[j] >= curr[0]:
+                    curr = (sums[j], j)
+            store[i] = curr[1]
+            sums[i] += curr[0]
+        answer = []
+        start = n - 1 if sums[n - 1] > sums[n - 2] else n - 2
+        while start is not None:
+            answer.append(start)
+            start = store[start]
+        ans = 0
+        for i in answer:
+            ans += nums[i]
+        return ans
             
-            if house not in memo:
-                x = calc(house + 2)
-                y = calc(house + 3)
-                memo[house] = max(x + nums[house], y + nums[house])
                 
-            return memo[house]
-        
-        return max(calc(0), calc(1))
