346fb5ef8de2c0c5cf6347ba981de95064eebfaf
Fikremariam Anteneh Asegie <105011492+fikreanteneh@users.noreply.github.com>
2023-11-20 23:49:34 +0300

    Added solution for Reconstruct Itinerary

--- #leetcode-solution/reconstruct-itinerary.py
+++ #leetcode-solution/reconstruct-itinerary.py
@@ -1,30 +1,17 @@
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        
         path = defaultdict(list)
         for departure, arrival in tickets:
             path[departure].append(arrival)
        
         for departure, arrival in path.items():
             arrival.sort(reverse = True)
-        self.target = len(tickets) + 1
         self.answer = []
-        self.visited = defaultdict(set)
-        def dfs(city):
-            self.answer.append(city)
-            
-            if len(self.answer) == self.target:
-                return self.answer
-            n = len(path[city])
-            for i in range(n - 1, -1, -1):
-                if i not in self.visited[city]:
-                    self.visited[city].add(i)
-                    travel = dfs(path[city][i])
-                    if travel:
-                        return travel
-                    self.visited[city].discard(i)
-            self.answer.pop()
-        
-        
-        return dfs("JFK")
-        
+        stack = ["JFK"]
+        while stack:
+            if path[stack[-1]]:
+                stack.append( path[stack[-1]].pop())
+            else:
+                self.answer.append( stack.pop() )
+        self.answer.reverse()    
+        return self.answer
