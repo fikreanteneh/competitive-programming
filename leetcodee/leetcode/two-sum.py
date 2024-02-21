bf8649689972f9275da418ab11cd92133edbf087
Fikremariam Anteneh Asegie <105011492+fikreanteneh@users.noreply.github.com>
2024-02-21 14:08:03 +0300

    Add solution for Two Sum

--- leetcodee/leetcode/two-sum.py
+++ leetcodee/leetcode/two-sum.py
@@ -1,6 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         value_index_dict = {}
+        
         for i, num in enumerate(nums):
             diff = target - num
             if diff in value_index_dict:
