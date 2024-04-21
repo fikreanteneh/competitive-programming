fc95110e2eea361c3843e8e1659e99e3d116cc35
Fikremariam Anteneh Asegie <105011492+fikreanteneh@users.noreply.github.com>
2024-04-21 20:30:35 +0300

    Time: 323 ms (98.18%), Space: 31.9 MB (19.21%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,5 +1,16 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        #Brilliant Solution
+        nums = set(nums)
+        best = 0
+        for x in nums:
+            if x - 1 not in nums:
+                y = x + 1
+                while y in nums:
+                    y += 1
+                best = max(best, y - x)
+        return best
+        # Union FInd Approach
         parents = {}
         ranks = defaultdict(lambda: 1)
         
