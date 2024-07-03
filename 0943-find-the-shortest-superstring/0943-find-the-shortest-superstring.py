dca5074f092ec6df2cfe67cbca6263644e523788
Fikremariam Anteneh Asegie <105011492+fikreanteneh@users.noreply.github.com>
2024-07-04 01:11:03 +0300

    Time: 680 ms (39.80%), Space: 101 MB (5.10%) - LeetHub

---
+++ 0943-find-the-shortest-superstring/0943-find-the-shortest-superstring.py
@@ -0,0 +1,103 @@
+class Solution:
+    def shortestSuperstring(self, words: List[str]) -> str:
+
+        n = len(words)
+        self.state = 0
+        self.target = (1<<n) - 1
+        
+        def findPrefix(word1, word2):
+            index = 0
+            if word2 in word1:
+                return ""
+            m = len(word1)
+            for i in range(min(len(word1),len(word2))):
+                if word1[m - i - 1:] == word2[:i + 1]:
+                    index = i + 1
+            return word2[index:]
+        
+        store = [["" for i in range(n + 1)] for j in range(n + 1)]
+        for i in range(n):
+            for j in range(i):
+                store[i][j] = findPrefix(words[i], words[j])
+                store[j][i] = findPrefix(words[j], words[i])
+        
+        def pickTheBest(possible, curr):
+            currBest = ("", float("inf"))
+            for string, prev in possible:
+                option = len(string) + len(store[prev][curr])
+                if option < currBest[1]:
+                    currBest = (string + store[prev][curr], option)
+            return (currBest[0], curr)
+        
+        @cache
+        def backtrack(state, curr):
+
+            if (state ^ (1<<curr)) == self.target:
+                return (words[curr], curr)
+            state ^= (1 << curr)
+            possible = []
+            for i in range(n):
+                if state & (1 << i):
+                    continue
+                possible.append(backtrack(state, i))
+            return pickTheBest(possible, curr)
+        
+        answer = "x"*241
+        for i in range(n):
+            word, _ = (backtrack(0, i))
+            if len(word) < len(answer):
+                answer = word
+        return answer
+               
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #Subset Solution
+        n = len(words)
+        self.state = 0
+        self.target = (1<<n) - 1
+        self.answer = "x"*241
+        
+        def findPrefix(word1, word2):
+            index = 0
+            if word2 in word1:
+                return ""
+            m = len(word1)
+            for i in range(min(len(word1),len(word2))):
+                if word1[m - i - 1:] == word2[:i + 1]:
+                    index = i + 1
+            return word2[index:]
+        
+        store = [["" for i in range(n)] for j in range(n)]
+        for i in range(n):
+            for j in range(i):
+                store[i][j] = findPrefix(words[i], words[j])
+                store[j][i] = findPrefix(words[j], words[i])
+                
+        
+        def backtrack(curr, index):
+            if len(curr) >= len(self.answer):
+                return
+            if self.state == self.target:
+                if len(curr) < len(self.answer):
+                    self.answer = curr
+                return
+            for i, word in enumerate(words):
+                if self.state & (1 << i):
+                    continue
+                self.state ^= (1 << i)
+                backtrack(curr + store[index][i], i)
+                self.state ^= (1 << i)
+        for i in range(n): 
+            self.state = 1 << i
+            backtrack(words[i], i)
+        return self.answer
+            
