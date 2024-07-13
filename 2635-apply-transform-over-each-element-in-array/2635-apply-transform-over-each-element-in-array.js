f1c06b88f847a16cf2bd81b35d4a81ea55b06627
Fikremariam Anteneh Asegie <105011492+fikreanteneh@users.noreply.github.com>
2024-07-13 21:28:08 +0300

    Time: 52 ms (55.23%), Space: 49.2 MB (8.23%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -4,6 +4,7 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
+    return arr.map((element, index) => fn(element, index))
     for (let i = 0; i < arr.length; i++){
         arr[i] = fn(arr[i], i)
     }
