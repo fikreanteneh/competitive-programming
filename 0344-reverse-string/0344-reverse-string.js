64bd3df14300e019d55c2f13e99c8eac0b67cf9f
Fikremariam Anteneh Asegie <105011492+fikreanteneh@users.noreply.github.com>
2024-07-14 10:38:14 +0300

    Time: 92 ms (8.82%), Space: 57.1 MB (39.72%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -3,6 +3,9 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
+    s.reverse()
+    return s
+    
     for (let i = 0; i < s.length / 2; i++) {
         let [first, last] = [s[i], s[s.length - i - 1]];
         s[i] = last;
