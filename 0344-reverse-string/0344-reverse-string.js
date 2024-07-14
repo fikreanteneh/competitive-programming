9fe789ea520b6eceac70e04bb9e2ecbaec2f269b
Fikremariam Anteneh Asegie <105011492+fikreanteneh@users.noreply.github.com>
2024-07-14 10:37:57 +0300

    Time: 76 ms (68.14%), Space: 57.3 MB (27.30%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.js
@@ -0,0 +1,12 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    for (let i = 0; i < s.length / 2; i++) {
+        let [first, last] = [s[i], s[s.length - i - 1]];
+        s[i] = last;
+        s[s.length - i - 1] = first;
+    }
+    return s.join("");
+};
