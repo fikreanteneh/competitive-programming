a9a8506f87469089a146d3cde20ed13df6239931
Fikremariam Anteneh Asegie <105011492+fikreanteneh@users.noreply.github.com>
2024-07-20 02:31:16 +0300

    Time: 65 ms (10.20%), Space: 16.7 MB (48.24%) - LeetHub

--- 0330-patching-array/0330-patching-array.py
+++ 0330-patching-array/0330-patching-array.py
@@ -2,14 +2,12 @@ class Solution:
     def minPatches(self, nums: List[int], n: int) -> int:
         patches = 0
         currSum = 0
+        nums.append(float("inf"))
         for i, num in enumerate(nums):
-            if currSum >= n:
-                break
             while currSum < num - 1 and currSum < n:
                 patches += 1
                 currSum += (currSum + 1)
             currSum += num
-        while currSum < n:
-            patches += 1
-            currSum += (currSum + 1)
+            if currSum >= n:
+                break
         return patches    
