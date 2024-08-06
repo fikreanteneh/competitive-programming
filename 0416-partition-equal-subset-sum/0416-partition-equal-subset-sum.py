562c53b3adbed8d604cd5343cf3b174f00088e1a
Fikremariam Anteneh Asegie <105011492+fikreanteneh@users.noreply.github.com>
2024-08-06 18:16:11 +0300

    Time: 625 ms (54.77%), Space: 157.4 MB (10.73%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,7 +1,9 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         n = len(nums)
-        target = sum(nums)/2
+        sums = sum(nums)
+        if sums%2 == 1:
+            return False
         
         @cache
         def solve(index, target):
@@ -11,4 +13,4 @@ class Solution:
                 return False
             return solve(index + 1, target - nums[index]) or solve(index + 1, target)
         
-        return solve(0, target)
+        return solve(0, sum(nums)/2)
